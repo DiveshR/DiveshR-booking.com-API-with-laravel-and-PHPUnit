4eedb80ad51b388b867fe1e4b3a406709eac06a3
DiveshR <divesh20july@gmail.com>
2023-12-18 21:36:02 +0530

    Adding User Profile DB and Structure and adding additional fields to users table

---
+++ database/migrations/2023_12_18_065912_add_additional_fields_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('display_name')->nullable()->after('id');
+            $table->string('phone_number')->nullable()->after('email_verified_at');
+            $table->string('photo')->after('phone_number')->nullable();
+            $table->timestamp('phone_verified_at')->nullable()->after('photo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
